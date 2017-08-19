9984d964db89a1dd62ac3163855b76b35bebb2a9
Royce <royce236@gmail.com>
2017-08-19 18:56:00 +0800

    Added bootstrap4 to settings.py and added some code to models.py

--- LadylynnSistersReservation_Website/home/models.py
+++ LadylynnSistersReservation_Website/home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class userAccount(models.Model):
+    
+
+    def __str__(self):
+        return 
+
+    def __unicode__(self):
+        return 
