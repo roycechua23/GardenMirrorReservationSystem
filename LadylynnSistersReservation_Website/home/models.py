9a952f2e6c658702bebd870acbe6f7676eb1aac3
Royce <royce236@gmail.com>
2017-08-20 23:09:12 +0800

    addition to home models.py

--- LadylynnSistersReservation_Website/home/models.py
+++ LadylynnSistersReservation_Website/home/models.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class userAccount(models.Model):
+class UserProfileInfo(models.Model):
     
-
+    user = models.OneToOneField(User)
+    
+    # please pip install pillow before using 
+    # Image field
+    profile_pic = models.ImageField(upload_to='profile_pics')
+    
     def __str__(self):
-        return 
-
-    def __unicode__(self):
-        return 
+        return self.user.username
