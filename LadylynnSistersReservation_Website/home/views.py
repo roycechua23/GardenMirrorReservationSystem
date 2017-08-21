143f1cfb296f1a6627a8783e7a2d031d15fa5f42
Royce <royce236@gmail.com>
2017-08-21 14:20:38 +0800

    Remaking the navbar for base.html

--- LadylynnSistersReservation_Website/home/views.py
+++ LadylynnSistersReservation_Website/home/views.py
@@ -3,5 +3,5 @@ from home.forms import UserForm
 # Create your views here.
 
 def index(request):
-    return render(request,"home/index.html",context=None)
+    return render(request,"home/base.html",context=None)
 
