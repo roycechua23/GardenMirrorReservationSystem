d0f1ee65ca4565d009ea48809a66ded3a25db8be
Royce <royce236@gmail.com>
2017-08-26 20:16:26 +0800

    Completed sign up and sign in functionality as of this commit. Currently fixing a problem regarding transmitting the user data to the home page.

--- LadylynnSistersReservation_Website/home/views.py
+++ LadylynnSistersReservation_Website/home/views.py
@@ -1,71 +1,27 @@
 from django.shortcuts import render
 from home.forms import UserForm,UserProfileInfoForm
+from home.models import User,UserProfileInfo
+
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate,login,logout
+from django.http import HttpResponseRedirect,HttpResponse
+from django.views.decorators.http import require_http_methods
+
 # Create your views here.
 
 def index(request):
-    
-    registered = False
-
-    if request.method == 'POST':
-
-        # Get info from "both" forms
-        # It appears as one form to the user on the .html page
-        user_form = UserForm(data=request.POST)
-        profile_form = UserProfileInfoForm(data=request.POST)
-
-        # Check to see both forms are valid
-        if user_form.is_valid() and profile_form.is_valid():
-
-            # Save User Form to Database
-            user = user_form.save()
-
-            # Hash the password
-            user.set_password(user.password)
-
-            # Update with Hashed password
-            user.save()
-
-            # Now we deal with the extra info!
-
-            # Can't commit yet because we still need to manipulate
-            profile = profile_form.save(commit=False)
-
-            # Set One to One relationship between
-            # UserForm and UserProfileInfoForm
-            profile.user = user
-
-            # Check if they provided a profile picture
-            if 'profile_pic' in request.FILES:
-                print('found it')
-                # If yes, then grab it from the POST form reply
-                profile.profile_pic = request.FILES['profile_pic']
-
-            # Now save model
-            profile.save()
-
-            # Registration Successful!
-            registered = True
-
-        else:
-            # One of the forms was invalid if this else gets called.
-            print(user_form.errors,profile_form.errors)
-
-    else:
-        # Was not an HTTP post so we just render the forms as blank.
-        user_form = UserForm()
-        profile_form = UserProfileInfoForm()
+    user_form = UserForm()
+    profile_form = UserProfileInfoForm()
 
     return render(request,"home/body.html",
                           context={'user_form':user_form,
                                    'profile_form':profile_form})
 
-def login(request):
-    pass # last test edit from Royce Branch
+def loadlogin(request):
+    return render(request,'home/login.html',{})
 
-
-def rtest(request):
-    return render(request,"home/index.html",context=None)
-
+@require_http_methods(["POST"])
 def register(request):
 
     registered = False
@@ -125,3 +81,49 @@ def register(request):
                           {'user_form':user_form,
                            'profile_form':profile_form,
                            'registered':registered})
+
+@require_http_methods(["POST"])
+def user_login(request):
+    
+    if request.method == "POST":
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username,password=password)
+
+        if user:
+            if user.is_active:
+                login(request,user)
+                u = UserProfileInfo.objects.filter(user__username=username)
+                request.session['user_id'] = u.id
+                return HttpResponseRedirect(reverse('home/{}'.format(u.id)))
+            else:
+                return HttpResponse("ACCOUNT NOT ACTIVE")
+        else:
+            print("Someone tried to login and failed")
+            print("Username: {} and password {}".format(username,password))
+            return HttpResponse("Invalid Login details supplied")
+    
+    else:
+        return render(request,'home/login.html',{})
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+@login_required
+def home(request,pk):
+    print(pk)
+    userinfo = User.objects.filter(id=pk)
+    userprofileinfo = UserProfileInfo.objects.filter(user__id=pk)
+    return render(request,"reservation/home.html",{'user':userinfo,'userprofilepic':userprofileinfo})
+
+@login_required
+def special(request):
+    return HttpResponse("You are logged in, Nice!")
+
+def rtest(request):
+    return render(request,"home/index.html",context=None)
+
