f4289530f8ee82b426efa979757aba1b4415ca0a
Royce <royce236@gmail.com>
2017-09-21 11:38:55 +0800

    Renaming the system - for Client

--- GardenMirrorReservation_Website/GardenMirrorReservation_Website/wsgi.py
+++ GardenMirrorReservation_Website/GardenMirrorReservation_Website/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for LadylynnSistersReservation_Website project.
+WSGI config for GardenMirrorReservation_Website project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LadylynnSistersReservation_Website.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GardenMirrorReservation_Website.settings")
 
 application = get_wsgi_application()
