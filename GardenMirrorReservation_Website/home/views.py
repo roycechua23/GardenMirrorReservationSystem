2574d6f0cfdc90b01b1ac8175f8e0ae8b1773a5b
Royce <royce236@gmail.com>
2017-10-04 18:35:08 +0800

    update

--- GardenMirrorReservation_Website/home/views.py
+++ GardenMirrorReservation_Website/home/views.py
@@ -16,6 +16,7 @@ from home.forms import Reservation, CateringPackage
 def index(request):
     user_form = UserForm()
     profile_form = UserProfileInfoForm()
+    u = User.objects.all()
 
     return render(request,"home/index.html",
                           context={'user_form':user_form,
