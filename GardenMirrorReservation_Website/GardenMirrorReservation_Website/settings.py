a6760953cc0519815ae15f7f71a6a909dc0cf776
Royce <royce236@gmail.com>
2017-10-06 01:42:55 +0800

    update userhome final urls

--- GardenMirrorReservation_Website/GardenMirrorReservation_Website/settings.py
+++ GardenMirrorReservation_Website/GardenMirrorReservation_Website/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap4',
     'home',
-    # 'corsheaders',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
 ]
 
