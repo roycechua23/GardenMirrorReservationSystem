b290f426aaba939be6d2d5ec7739e6980bf900a1
Royce <royce236@gmail.com>
2017-10-06 02:51:55 +0800

    update on settings

--- GardenMirrorReservation_Website/GardenMirrorReservation_Website/settings.py
+++ GardenMirrorReservation_Website/GardenMirrorReservation_Website/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'corsheaders.middleware.CorsPostCsrfMiddleware'
 ]
