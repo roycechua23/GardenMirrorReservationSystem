d7fde9853430b86b8d45f472cd38de361050d4c1
Royce <royce236@gmail.com>
2017-10-06 06:35:35 +0800

    updates

--- GardenMirrorReservation_Website/home/views.py
+++ GardenMirrorReservation_Website/home/views.py
@@ -11,7 +11,7 @@ from django.contrib.auth import authenticate,login,logout
 from django.http import HttpResponseRedirect,HttpResponse, JsonResponse
 from django.views.decorators.http import require_http_methods
 from home.forms import Reservation, CateringPackage
-from django.views.decorators.csrf import csrf_exempts
+
 
 # for rest framework
 from rest_framework import viewsets
@@ -19,6 +19,7 @@ from home.serializers import *
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.views.decorators.csrf import csrf_exempts
 
 class UserViewSet(viewsets.ModelViewSet):
     """
