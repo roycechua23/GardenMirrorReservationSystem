48a61f52600281dfb0a910b7b341a6da2154e676
Royce <royce236@gmail.com>
2017-10-06 09:53:18 +0800

    update on cancel reservations

--- GardenMirrorReservation_Website/static/home/js/cancel_reservation.js
+++ GardenMirrorReservation_Website/static/home/js/cancel_reservation.js
@@ -16,6 +16,7 @@ $(document).ready(function() {
                 success: function(data) {
                     console.log(data.message);
                     $('#deletesuccess').toggle().fadeIn(300);
+                    $('#events').val().remove();
                 },
                 error: function(error) {
                     console.log("Something went wrong.");
